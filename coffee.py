dcad2f2f28b693e86a36d0fc15bcf382a5b36367
Anna Rift <ultimateartificer@gmail.com>
2026-04-23 18:32:38 -0600

    Also print data after today's payment

--- coffee.py
+++ coffee.py
@@ -158,12 +158,18 @@ def main():
         print()
 
     # Print the data as it currently stands, before making today's paying decision
+    print("Payment data so far:")
     print(coffee_data)
     print()
 
     # Select and report today's payer
     todays_payer = coffee_data.pick_payer_and_pay()
     print(f">> {todays_payer.name} is paying for everyone's coffee today!")
+    print()
+
+    # Print data after today's order
+    print("Payment data after today:")
+    print(coffee_data)
 
     # Save updated state to file
     coffee_data.save_to_file(path)

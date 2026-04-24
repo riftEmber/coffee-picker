1c07460746e0a98e7907057a6060514dc9e5f358
Anna Rift <ultimateartificer@gmail.com>
2026-04-23 18:14:57 -0600

    Move almost all logic from main to CoffeeData

--- coffee.py
+++ coffee.py
@@ -5,7 +5,6 @@ import errno
 import os
 import pickle
 from dataclasses import dataclass, field
-from typing import Self
 from pathlib import Path
 from tabulate import tabulate
 
@@ -25,6 +24,60 @@ class CoffeeDrinker:
 @dataclass
 class CoffeeData:
     drinkers: list[CoffeeDrinker] = field(default_factory=list)
+
+    def restore_from_file(self, path) -> None:
+        """Restore saved data from file"""
+        if path.exists():
+            if path.stat().st_size:
+                with open(path, "rb") as file:
+                    self.drinkers = pickle.load(file)
+
+    def save_to_file(self, path) -> None:
+        """Save this data to file, overwriting previous"""
+        with open(path, "wb") as file:
+            pickle.dump(self.drinkers, file)
+
+    def initialize_on_cli(self) -> None:
+        """Fill in initial data by prompting for command line input.
+
+        Expects that there is no data stored in this instance already.
+        """
+        assert not self.drinkers
+
+        num_drinkers = int(request_input("How many coffee drinkers are in your group?"))
+        for idx in range(1, num_drinkers + 1):
+            print(f"Getting data for drinker {idx}...")
+            name = request_input("Name:")
+            drink_name = request_input("Favorite drink:")
+            drink_cost = float(request_input("Drink cost:"))
+            times_covered_by_others = int(
+                request_input("Times paid for by others (default 0)") or 0
+            )
+            drinker = CoffeeDrinker(
+                name, drink_name, drink_cost, times_covered_by_others
+            )
+            self.drinkers.append(drinker)
+
+        print(
+            f"Data entered for all {num_drinkers} drinkers! This will be saved for next time."
+        )
+
+    def pick_payer_and_pay(self) -> CoffeeDrinker:
+        """Select who will pay for today's coffee order, and record payment.
+
+        Selects the person who has had the most paid for on their behalf so far.
+        """
+
+        todays_payer = max(
+            self.drinkers,
+            key=lambda drinker: drinker.drink_cost * drinker.times_covered_by_others,
+        )
+
+        for drinker in self.drinkers:
+            if drinker is not todays_payer:
+                drinker.times_covered_by_others += 1
+
+        return todays_payer
 
     def __str__(self) -> str:
         """Get a table representation of the stored data."""
@@ -53,10 +106,17 @@ class CoffeeData:
         return tabulate(table_rows, headers=table_headers)
 
 
-def request_input(prompt: str) -> str:
-    """Print a message to the console, then ask for input with a little prompt symbol."""
-    request_input(prompt)
-    return input("> ")
+def request_input(prompt: str, expected_type: type = str) -> str:
+    """Print a message to the console, then prompt for user input of the expected type"""
+    print(prompt)
+    while True:
+        user_input_str = input("> ")
+        try:
+            return expected_type(user_input_str)
+        except ValueError:
+            print(
+                f"Could not convert input '{user_input_str}' to expected type ({expected_type}), please try again"
+            )
 
 
 def main():
@@ -66,57 +126,33 @@ def main():
     parser.add_argument("data_filename")
     args = parser.parse_args()
 
-    coffeeData = CoffeeData()
+    # Validate provided path
+    path = Path(args.data_filename)
+    if path.is_dir():
+        raise IsADirectoryError(
+            errno.EISDIR, os.strerror(errno.EISDIR), args.data_filename
+        )
 
     # Restore state from provided file, if available
-    path = Path(args.data_filename)
-    if path.exists():
-        if path.is_dir():
-            raise IsADirectoryError(
-                errno.EISDIR, os.strerror(errno.EISDIR), args.data_filename
-            )
-        if path.stat().st_size:
-            with open(path, "rb") as file:
-                coffeeData = pickle.load(file)
+    coffeeData = CoffeeData()
+    coffeeData.restore_from_file(path)
 
     # Initialize a new session if we have no previous data
     if not coffeeData.drinkers:
         print(f"Starting new coffee drinker tracking in '{args.data_filename}'")
-        num_drinkers = int(request_input("How many coffee drinkers are in your group?"))
-        for idx in range(1, num_drinkers + 1):
-            print(f"Getting data for drinker {idx}...")
-            name = request_input("Name:")
-            drink_name = request_input("Favorite drink:")
-            drink_cost = float(request_input("Drink cost:"))
-            times_covered_by_others = int(
-                request_input("Times paid for by others (default 0)") or 0
-            )
-            drinker = CoffeeDrinker(
-                name, drink_name, drink_cost, times_covered_by_others
-            )
-            coffeeData.drinkers.append(drinker)
-        print(
-            f"Data entered for all {num_drinkers} drinkers! This will be saved for next time."
-        )
+        coffeeData.initialize_on_cli()
         print()
 
     # Print the data as it currently stands, before making today's paying decision
     print(coffeeData)
     print()
 
-    # Select the person who has had the most paid on their behalf (so far) to pay for coffee today
-    todays_payer = max(
-        coffeeData.drinkers,
-        key=lambda drinker: drinker.drink_cost * drinker.times_covered_by_others,
-    )
-    print(f"{todays_payer.name} will be paying for everyone's coffee today!")
-    for drinker in coffeeData.drinkers:
-        if drinker is not todays_payer:
-            drinker.times_covered_by_others += 1
+    # Select and report today's payer
+    todays_payer = coffeeData.pick_payer_and_pay()
+    print(f"{todays_payer.name} is paying for everyone's coffee today!")
 
     # Save updated state to file
-    with open(path, "wb") as file:
-        pickle.dump(coffeeData, file)
+    coffeeData.save_to_file(path)
 
 
 if __name__ == "__main__":
